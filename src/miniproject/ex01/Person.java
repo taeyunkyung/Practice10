ba5bb522be1805ca734408b0c0cc58b5c4318d48
경태윤 <xodbsrud320@gmail.com>
2021-12-18 14:33:31 +0900

    01

--- src/miniproject/ex01/Person.java
+++ src/miniproject/ex01/Person.java
@@ -1,4 +1,4 @@
-package miniproject;
+package miniproject.ex01;
 
 public class Person {
 
--- src/miniproject/ex01/PhoneApp.java
+++ src/miniproject/ex01/PhoneApp.java
@@ -1,4 +1,4 @@
-package miniproject;
+package miniproject.ex01;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
